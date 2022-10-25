efb24c4ac4947952b66c99942dbd10daf4e2dcfa
19di32qa <87597749+19di32qa@users.noreply.github.com>
2022-10-25 13:50:24 +0200

    actions in selenium

---
+++ src/test/java/locators/exercise1.java
@@ -0,0 +1,90 @@
+package locators;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class exercise1 {
+    WebDriver driver;
+    WebElement element;
+    @Before
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver","resources/chromedriver.exe");
+        driver = new ChromeDriver();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
+    }
+
+    @Test
+    public void testSelectedOption() {
+//        driver.get("https://the-internet.herokuapp.com/dropdown");
+//        driver.findElement(By.cssSelector("#dropdown")).click();
+//        driver.findElement(By.cssSelector("[value=\"1\"]")).click();
+//        Assert.assertFalse(driver.findElement(By.cssSelector("[value=\"2\"]")).isSelected());
+//        Assert.assertTrue((driver.findElement(By.cssSelector("[value=\"1\"]")).isSelected()));
+        driver.get("https://the-internet.herokuapp.com/dropdown");
+        driver.findElement(By.cssSelector("#dropdown")).click();
+        WebElement option1 = driver.findElement(By.cssSelector("[value=\"1\"]"));
+        WebElement option2 = driver.findElement(By.cssSelector("[value=\"2\"]"));
+        option1.click();
+        Assert.assertTrue(option1.isSelected());
+        Assert.assertFalse(option2.isSelected());
+    }
+
+    @Test
+    public void testHover() {
+        driver.get("https://the-internet.herokuapp.com/hovers");
+        Actions action = new Actions(driver);
+        element = driver.findElement(By.cssSelector(".row:nth-child(2)"));
+        WebElement target = driver.findElement(By.cssSelector(".figure:first-of-type"));
+        action.moveToElement(target).build().perform();
+        WebElement elem2 = driver.findElement(By.cssSelector(".figure:first-of-type h5"));
+        //String str = elem2.getText();
+        //System.out.println(str.equals("name: user1"));
+        Assert.assertTrue(elem2.getText().equals("name: user1"));
+        Assert.assertTrue(elem2.isDisplayed());
+    }
+    @Test
+    public void testContextMenuAndAlert() {
+        driver.get("https://the-internet.herokuapp.com/context_menu");
+        element = driver.findElement(By.cssSelector("#hot-spot"));
+        Actions action = new Actions(driver);
+        action.contextClick(element).build().perform();
+
+        driver.switchTo().alert().accept();
+    }
+    @Test
+    public void testKeyEnter() {
+        driver.get("https://the-internet.herokuapp.com/key_presses");
+        element = driver.findElement(By.cssSelector("#target"));
+        Actions action = new Actions(driver);
+        action.sendKeys(element, Keys.ARROW_RIGHT).build().perform();
+        WebElement elem2 = driver.findElement(By.id("result"));
+        boolean res = checkString(elem2.getText(), "RIGHT");
+        Assert.assertTrue(res);
+
+    }
+    public boolean checkString(String str1, String str2) {
+        if (str1.indexOf(str2) != -1) {
+            return true;
+        }
+        return false;
+    }
+    @Test
+    public void checkAttributes() {
+        driver.get("https://ultimateqa.com/simple-html-elements-for-automation/");
+        element = driver.findElement(By.cssSelector("[href=\"/link-success/\"]"));
+        Assert.assertTrue(element.getAttribute("href").equals("https://ultimateqa.com/link-success/"));
+        Assert.assertTrue(element.getCssValue("background-origin").equals("padding-box"));
+    }
+}
